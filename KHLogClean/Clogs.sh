8f3cb26e30fb85ffba537724ed7b6f7ba5cf1ebe
WoShiDaTiDu <769944715@qq.com>
2018-05-07 16:59:21 +0800

    2018/5/7_16_59
    upgrade

--- KHLogClean/Clogs.sh
+++ KHLogClean/Clogs.sh
@@ -5,34 +5,65 @@ PATH=$JAVA_HOME/bin:$JRE_HOME/bin:$PATH
 export JAVA_HOME CLASS_PATH PATH
 export LD_LIBRARY_PATH=$JRE_HOME/lib:$LD_LIBRARY_PATH
 export > /tmp/export.tmp.log
-cd /usr/local/tomcat/apache-tomcat-mobile/bin/
-./shutdown.sh
+shopt -s extglob
+cd /root/
+d=$(date +%Y-%m-%d_%H:%M:%S)
+v=$(wc tt.txt -l|awk '{print $1}')
+((v=$v+1))
+echo $v 	$d 	'>>>>>>start>>>>>' >> tt.txt
+cd /usr/games/Governor/control/
+./mobile.sh
+./front.sh
+./middle.sh
+./backend.sh
+./video.sh
 sleep 10
 ps -ef|grep mobile |grep -v 'grep'|awk '{print $2}'|xargs kill -9
+ps -ef|grep front |grep -v 'grep'|awk '{print $2}'|xargs kill -9
+ps -ef|grep middle |grep -v 'grep'|awk '{print $2}'|xargs kill -9
+ps -ef|grep backend |grep -v 'grep'|awk '{print $2}'|xargs kill -9
+ps -ef|grep video |grep -v 'grep'|awk '{print $2}'|xargs kill -9
+sleep 5
+ps -ef|grep anychat|sed -n '1p;1q'|awk '{print $2}'|xargs kill -9
+ps -ef|grep anychat|sed -n '1p;1q'|awk '{print $2}'|xargs kill -9
+sleep 5
+ps -ef|grep nginx|grep master|awk '{print $2}'|xargs kill -9
+ps -ef|grep nginx|grep worker|awk '{print $2}'|xargs kill -9
+sleep 5
+cd /root/
+d=$(date +%Y-%m-%d_%H:%M:%S)
+v=$(wc tt.txt -l|awk '{print $1}')
+((v=$v+1))
+echo $v 	$d 	'>>>>>>clean>>>>>' >> tt.txt
 cd /usr/local/tomcat/apache-tomcat-mobile/logs/
 rm -rf * 
-sleep 10
-cd /usr/local/tomcat/apache-tomcat-mobile/bin/
-./startup.sh
-sleep 10
-cd /usr/local/tomcat/apache-tomcat-video/bin/
-./shutdown.sh
-sleep 10
-ps -ef|grep video |grep -v 'grep'|awk '{print $2}'|xargs kill -9
+cd /usr/local/tomcat/apache-tomcat-front/logs/
+rm -rf *
+cd /usr/local/tomcat/apache-tomcat-middle/logs/
+rm -rf *
+cd /usr/local/tomcat/apache-tomcat-backend/logs/
+rm -rf !(htLog)
 cd /usr/local/tomcat/apache-tomcat-video/logs/
 rm -rf *
-sleep 10
-cd /usr/local/tomcat/apache-tomcat-video/bin/
-./startup.sh
 cd /usr/local/tomcat/anychatCore61/bin/server/temp
 rm -rf *
-ps -ef|grep nginx|grep master|awk '{print $2}'|xargs kill -9
-sleep 10
-ps -ef|grep nginx|grep worker|awk '{print $2}'|xargs kill -9
-sleep 10
-cd /usr/local/nginx/logs
+cd /usr/local/nginx/logs/
 rm -rf *
-touch a.txt creat file
+sleep 10
 cd /usr/local/nginx/sbin/
 ./nginx
-sleep 1
+sleep 10
+cd /usr/local/tomcat/anychatCore61/bin/server
+./runanychatcoreserver.sh
+sleep 5
+cd /usr/games/Governor/control/
+./upmobile.sh
+./upfront.sh
+./upmiddle.sh
+./upbackend.sh
+./upvideo.sh
+cd /root/
+d=$(date +%Y-%m-%d_%H:%M:%S)
+v=$(wc tt.txt -l|awk '{print $1}')
+((v=$v+1))
+echo $v 	$d 	'>>>>>>stop>>>>>' >> tt.txt
